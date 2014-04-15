90d1cb20e83d6b8eee7d234dbad563178ce3c374
Andreas Olsson <olsson.andreas@gmail.com>
2014-04-15 14:08:35 +0200

    [FIX] Commented three require_once since this did not work with 6.2

--- Classes/class.tx_cbgooglemaps_addFlexformFields.php
+++ Classes/class.tx_cbgooglemaps_addFlexformFields.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 /**
- * Class with methods to extend flexforms with user fields 
+ * Class with methods to extend flexforms with user fields
  * @package				Tx_Cbgooglemaps_addFlexformFields
  * @path 				Tx_Cbgooglemaps_addFlexformFields.php
  * @version				1.0: Tx_Cbgooglemaps_addFlexformFields.php,  03.07.2011
@@ -9,26 +9,26 @@
  */
 
 class tx_cbgooglemaps_addFlexformFields
-{	
+{
 	public function addGeocodingButton($config)
-	{	
-		$fiedset = '';		
+	{
+		$fiedset = '';
 		$fiedset =  '<div class="cbgm_geocoding">';
 		$fiedset .= '<input type="button" onclick="doGeocoding(\''.$config['row']['uid'].'\')" value="fetch coordinates from above-mentioned address">';
 		$fiedset .= '<input type="button" onclick="displayLocation(\''.$config['row']['uid'].'\')" value="display current location">';
 		$fiedset .= '<div id="cbgm_previewLocation"></div>';
 		$fiedset .= '</div>';
-		
+
 		return $fiedset;
 	}
 
 	public function addPreviewButton($config)
 	{
 		// load current extension typoscript and provide settings to $this->conf
-		require_once (PATH_t3lib.'class.t3lib_page.php');
-		require_once (PATH_t3lib.'class.t3lib_tstemplate.php');
-		require_once (PATH_t3lib.'class.t3lib_tsparser_ext.php');
-		
+		// require_once (PATH_t3lib.'class.t3lib_page.php');
+		// require_once (PATH_t3lib.'class.t3lib_tstemplate.php');
+		// require_once (PATH_t3lib.'class.t3lib_tsparser_ext.php');
+
 		$sysPageObj = t3lib_div::makeInstance('t3lib_pageSelect');
 		$rootLine = $sysPageObj->getRootLine($config['row']['pid']);
 		$TSObj = t3lib_div::makeInstance('t3lib_tsparser_ext');
@@ -37,8 +37,8 @@ class tx_cbgooglemaps_addFlexformFields
 		$TSObj->runThroughTemplates($rootLine);
 		$TSObj->generateConfig();
 		$this->conf = $TSObj->setup['plugin.']['tx_cbgooglemaps.'];
-		
-		$fiedset = '';		
+
+		$fiedset = '';
 		$fiedset =  '<div class="cbgm_preview">';
 		$fiedset .= '<input type="button" onclick="displayPreview(\''
 				  . $config['row']['uid'].'\',\''
@@ -48,7 +48,7 @@ class tx_cbgooglemaps_addFlexformFields
 				  . 'value="display current map">';
 		$fiedset .= '<div id="cbgm_previewMap"></div>';
 		$fiedset .= '</div>';
-		
+
 		return $fiedset;
 	}
 }

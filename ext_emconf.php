86f9f568e8219aded1ceb23903bbed2ca1fc4fd2
Mats Svensson <mats@pixelant.se>
2017-08-16 10:04:36 +0200

    [UPDATE] 3.2.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cbgooglemaps".
  *
- * Auto generated 16-09-2016 13:08
+ * Auto generated 09-11-2016 09:16
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,29 +11,29 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Quick Google Maps',
-	'description' => 'Include google maps to the frontend, in a quick and easy way - as content element, by typoscript or even with fluid cObject. (based on google maps api 3)',
-	'category' => 'plugin',
-	'version' => '3.2.5',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearcacheonload' => false,
-	'author' => 'Christian Brinkert',
-	'author_email' => 'ChristianBrinkert@googlemail.com',
-	'author_company' => '',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '6.2.0-7.99.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
+  'title' => 'Quick Google Maps',
+  'description' => 'Include google maps to the frontend, in a quick and easy way - as content element, by typoscript or even with fluid cObject. (based on google maps api 3)',
+  'category' => 'plugin',
+  'version' => '3.2.6',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearcacheonload' => false,
+  'author' => 'Christian Brinkert',
+  'author_email' => 'ChristianBrinkert@googlemail.com',
+  'author_company' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.2.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
 );
 
